f024dd5f271268053dee6c6f24cd4cd3e76b3937
Eugene <eugenijb@gmail.com>
2021-02-28 17:56:17 +0200

    added documentation

--- lib/ScrollSync.d.ts
+++ lib/ScrollSync.d.ts
@@ -1,6 +1,28 @@
 /// <reference types="react" />
 import { StyleProp, ViewStyle } from 'react-native';
-export declare function ScrollSync({ rowItems, containerStyle }: {
+export interface ScrollSyncProps {
+    /** Array of arrays with custom {@link JSX.Element} */
     rowItems: JSX.Element[][];
+    /** Custom style for ScrollSync container */
     containerStyle?: StyleProp<ViewStyle>;
-}): JSX.Element;
+    /** Choose Horizontal or Vertical type, default is Horizontal */
+    type?: 'horizontal' | 'vertical';
+}
+/**
+ * ScrollSync component
+ * Code example:
+ * ```jsx
+ * // Create Array of Arrays with items (range function from lodash)
+ * const rowItems: JSX.Element[][] = range(4).map(rowI => range(Math.random() * 20 + 10).map(index => (
+ *   <View key={`row${rowI}-${index}`}><Text>Test</Text></View>
+* )));
+* // Styles for View container for ScrollViews
+* const styles = StyleSheet.create({
+*   scrollSync: { height: 150, margin: 10, padding: 2, borderRadius: 10, borderWidth: 1, borderColor: 'gray' },
+* });
+* ...
+ * // use ScrollSync component in render
+ * <ScrollSync rowItems={rowItems} containerStyle={styles.scrollSync} />
+ * ```
+ */
+export declare function ScrollSync({ rowItems, containerStyle, type }: ScrollSyncProps): JSX.Element;
--- src/ScrollSync.tsx
+++ src/ScrollSync.tsx
@@ -3,33 +3,69 @@ import { StyleProp, ViewStyle } from 'react-native';
 
 import { NativeScrollEvent, NativeSyntheticEvent, ScrollView, View } from 'react-native';
 
-export function ScrollSync({rowItems, containerStyle}: {rowItems: JSX.Element[][], containerStyle?: StyleProp<ViewStyle>}) {
+// Props for ScrollSync component
+export interface ScrollSyncProps {
+    /** Array of arrays with custom {@link JSX.Element} */
+    rowItems: JSX.Element[][];
+    /** Custom style for ScrollSync container */
+    containerStyle?: StyleProp<ViewStyle>;
+    /** Choose Horizontal or Vertical type, default is Horizontal */
+    type?: 'horizontal' | 'vertical';
+}
+
+/**
+ * ScrollSync component
+ * Code example:
+ * ```jsx
+ * // Create Array of Arrays with items (range function from lodash)
+ * const rowItems: JSX.Element[][] = range(4).map(rowI => range(Math.random() * 20 + 10).map(index => (
+ *   <View key={`row${rowI}-${index}`}><Text>Test</Text></View>
+* )));
+* // Styles for View container for ScrollViews
+* const styles = StyleSheet.create({
+*   scrollSync: { height: 150, margin: 10, padding: 2, borderRadius: 10, borderWidth: 1, borderColor: 'gray' },
+* });
+* ...
+ * // use ScrollSync component in render
+ * <ScrollSync rowItems={rowItems} containerStyle={styles.scrollSync} />
+ * ```
+ */
+export function ScrollSync({ rowItems, containerStyle, type = 'horizontal' }: ScrollSyncProps) {
     const [contentPosition, setContentPosition] = React.useState(-1);
-    const [scrollWidths, setScrollWidths] = React.useState<Map<number, number>>(new Map());
+    const [scrollLengths, setScrollLengths] = React.useState<Map<number, number>>(new Map());
     const itemsRef = React.useRef<Array<ScrollView | null>>([]);
+    /**
+     * Change positions of other ScrollViews on each scroll
+     * @param event Event of scroll
+     * @param num Row number
+     */
     const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>, num: number) => {
         if (contentPosition !== num) {
             event.preventDefault();
             return;
         }
-        const scrollableCoord = event.nativeEvent.contentOffset.x;
-        const viewWidth = event.nativeEvent.layoutMeasurement.width;
-        const contentWidth = event.nativeEvent.contentSize.width;
-        const percentScroll = scrollableCoord / (contentWidth - viewWidth)
+        const scrollableCoord = type==='horizontal' ? event.nativeEvent.contentOffset.x : event.nativeEvent.contentOffset.y;
+        const viewSize = type==='horizontal' ? event.nativeEvent.layoutMeasurement.width : event.nativeEvent.layoutMeasurement.height;
+        const contentSize = type==='horizontal' ? event.nativeEvent.contentSize.width : event.nativeEvent.layoutMeasurement.height;
+        const percentScroll = scrollableCoord / (contentSize - viewSize)
         for (let index = 0; index < itemsRef.current.length; index++) {
             if (index === num) continue;
             const element = itemsRef.current[index];
             if (element) {
-                const elWidth = (scrollWidths.get(index) || 0) - viewWidth;
+                const elWidth = (scrollLengths.get(index) || 0) - viewSize;
                 const scrollOffset = percentScroll * elWidth;
                 element.scrollTo({ x: scrollOffset })
             }
-
         }
     };
-    const storeContentWidth = (w: number, num: number) => {
-        scrollWidths.set(num, w);
-        setScrollWidths(scrollWidths);
+    /**
+     * 
+     * @param length 
+     * @param num 
+     */
+    const storeContentLength = (length: number, num: number) => {
+        scrollLengths.set(num, length);
+        setScrollLengths(scrollLengths);
     }
     return (
         <View style={containerStyle}>
@@ -37,11 +73,11 @@ export function ScrollSync({rowItems, containerStyle}: {rowItems: JSX.Element[][
                 <ScrollView
                     key={`scRow_${rowI}`}
                     ref={(el: ScrollView | null) => itemsRef.current[rowI] = el}
-                    onContentSizeChange={(w: number) => storeContentWidth(w, rowI)}
+                    onContentSizeChange={(w: number, h: number) => storeContentLength(type==='horizontal' ? w : h, rowI)}
                     onScrollBeginDrag={() => setContentPosition(rowI)}
                     onScrollAnimationEnd={() => setContentPosition(-1)}
                     onScroll={(e: NativeSyntheticEvent<NativeScrollEvent>) => onScroll(e, rowI)}
-                    horizontal
+                    horizontal={type==='horizontal'}
                     showsHorizontalScrollIndicator={false}
                     showsVerticalScrollIndicator={false}
                     scrollEventThrottle={16}>
